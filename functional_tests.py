b88963bd4d6b6d5e0d07784a419a30813f0c5ce1
karlorg <karlorg@users.noreply.github.com>
2014-11-24 23:15:52 +0000

    Redirect after POST, and show all items in template

--- functional_tests.py
+++ functional_tests.py
@@ -11,6 +11,21 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    # I don't know why, but this prevents send_keys() from sending crazy
+    # nonsense strings on my machine
+    def do_send_keys(self, textbox, term):
+        while True:
+            textbox.clear()  # for when we loop and search more than once
+            i = 0
+            while i < len(term):
+                textbox.send_keys(term[i:i+10])
+                i += 10
+            actual_text = textbox.get_attribute('value')
+            if actual_text == term:
+                break
+            else:
+                pass  # retry because of rare send_keys() problem
+
     def check_for_row_in_list_table(self, row_text):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
@@ -35,7 +50,7 @@ class NewVisitorTest(unittest.TestCase):
 
         # She types "Buy peacock feathers" into a text box (Edith's
         # hobby is tying fly-fishing lures)
-        inputbox.send_keys('Buy peacock feathers')
+        self.do_send_keys(inputbox, 'Buy peacock feathers')
 
         # When she hits enter, the page updates, and now the page
         # lists "1: Buy peacock feathers" as an item in a to-do list
@@ -46,7 +61,7 @@ class NewVisitorTest(unittest.TestCase):
         # item. She enters "Use peacock feathers to make a fly" (Edith
         # is very methodical)
         inputbox = self.browser.find_element_by_id('id_new_item')
-        inputbox.send_keys('Use peacock feathers to make a fly')
+        self.do_send_keys(inputbox, 'Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
 
         # The page updates again, and now shows both items on her list
