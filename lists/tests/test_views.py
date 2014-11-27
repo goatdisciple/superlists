1e94c825f35ea11d3c91a2219c8a1bacefc9ed2b
goatdisciple <goatdisciple@users.noreply.github.com>
2014-11-27 17:34:11 +0000

    new form for items on existing list

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -3,10 +3,12 @@ from django.http import HttpRequest
 from django.template.loader import render_to_string
 from django.test import TestCase
 from django.utils.html import escape
+from unittest import skip
 
 from lists.forms import EMPTY_ITEM_ERROR, ItemForm
 from lists.models import Item, List
 from lists.views import home_page
+
 
 class HomePageTest(TestCase):
     maxDiff = None
@@ -19,6 +21,7 @@ class HomePageTest(TestCase):
         response = self.client.get('/')
         self.assertIsInstance(response.context['form'], ItemForm)
         
+
 class ListViewTest(TestCase):
 
     def post_invalid_input(self):
@@ -101,6 +104,20 @@ class ListViewTest(TestCase):
     def test_for_invalid_input_shows_error_on_page(self):
         response = self.post_invalid_input()
         self.assertContains(response, escape(EMPTY_ITEM_ERROR))
+
+    @skip
+    def test_duplicate_item_validation_errors_end_up_on_lists_page(self):
+        list1 = List.objects.create()
+        item1 = Item.objects.create(list=list1, text='textey')
+        response = self.client.post(
+            '/lists/%d/' % (list1.id,),
+            data={'text': 'textey'}
+        )
+
+        expected_error = escape("You've already got this in your list")
+        self.assertContains(response, expected_error)
+        self.assertTemplateUsed(response, 'list.html')
+        self.assertEqual(Item.objects.all().count(), 1)
 
 
 class NewListTest(TestCase):
